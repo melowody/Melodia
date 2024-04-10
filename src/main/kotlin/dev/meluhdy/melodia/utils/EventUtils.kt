2b3442d1ac037456cec8b831f8b4003498dd7b5b
Melody <melodydevofficial@gmail.com>
2024-04-10 00:02:45 -0400

    Add comments to the event utils lol

--- src/main/kotlin/dev/meluhdy/melodia/utils/EventUtils.kt
+++ src/main/kotlin/dev/meluhdy/melodia/utils/EventUtils.kt
@@ -3,9 +3,15 @@ package dev.meluhdy.melodia.utils
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
 
+/**
+ * A collection of utils regarding Bukkit Events
+ */
 @Suppress("unused")
 object EventUtils {
 
+    /**
+     * Call a given event
+     */
     fun callEvent(event: Event) {
         Bukkit.getPluginManager().callEvent(event)
     }
