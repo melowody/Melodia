349e818800f1a63a96e60285c743e1f32816676b
Melody <melodydevofficial@gmail.com>
2025-04-29 16:17:12 -0400

    Added GUI classes

--- src/main/kotlin/dev/meluhdy/melodia/gui/MelodiaGUIItem.kt
+++ src/main/kotlin/dev/meluhdy/melodia/gui/MelodiaGUIItem.kt
@@ -1,4 +1,12 @@
 package dev.meluhdy.melodia.gui
 
-class MelodiaGUIItem {
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+import java.util.function.Consumer
+
+data class MelodiaGUIItem(val position: Int, val item: ItemStack, val clickFunc: Consumer<in InventoryClickEvent>)
+
+fun Inventory.addItem(item: MelodiaGUIItem) {
+    this.setItem(item.position, item.item)
 }
