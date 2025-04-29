baa0aef2f2b6ca0ec13ee063a71149345d101779
Melody <melodydevofficial@gmail.com>
2025-04-29 18:55:28 -0400

    Idk

--- src/test/kotlin/dev/meluhdy/melodia/MelodiaPluginTest.kt
+++
@@ -1,9 +0,0 @@
-package dev.meluhdy.melodia
-
-import dev.meluhdy.melodia.command.MelodiaCommand
-
-open class MelodiaPluginTest : MelodiaPlugin() {
-
-    override fun getCommands(): ArrayList<MelodiaCommand> = arrayListOf()
-
-}
